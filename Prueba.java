a212a50ceaa777cd2780a321e8c9aa2c4dbbc5dd
Sergio <sergio.rodpri@gmail.com>
2024-09-24 10:20:48 +0200

    Ahora se muestra mi cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]){
 
-		System.out.println("Rodriguez Prieto");
 		System.out.println("18 años-2006");
 		System.out.println("Lasaña");
+		System.out.println("Quevedo");
 	}
 
 }
